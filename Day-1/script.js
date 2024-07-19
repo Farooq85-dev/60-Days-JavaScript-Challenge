b8da0c63eff14a8c280c7dd227417f111a6e2318
Muhammad Farooq <famuhammad907@gmail.com>
2024-07-19 18:30:52 +0500

    Day-1

---
+++ Day-1/script.js
@@ -0,0 +1,20 @@
+console.log("Hello Farooq!");
+
+
+
+//Js Engine? 
+/* A JavaScript engine is a computer program that executes JavaScript code and
+converts it into computer understandable language.*/
+
+// Top Browser that use Some JavaScript engines.
+// Google Chrome ===> V8 Engine
+// MS Edge ===> Chakra Engine
+// Mozilla Firefox ===> Soider Monkey Engine
+// Safari ===> V8 Engine
+
+// Top JavaScript Runtimes
+//Node ===> Much faster than all others
+//Dino
+//Bun 
+
+//To run this node open terminal and write node and then write your js file
