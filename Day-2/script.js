c2b87e8425c03b1ca0ac0d212050f02eaca71f6b
Muhammad Farooq <famuhammad907@gmail.com>
2024-07-20 13:12:45 +0500

    Day-2

---
+++ Day-2/script.js
@@ -0,0 +1,10 @@
+//EcmaScript Stang for ===> European Manufacturers Association
+
+/* EcmaScript is standard for scripting languges including JScript, Action Script and JavaScript. 
+ECMAScript is commonly used for client-side scripting on the World Wide Web, and it is increasingly
+being used for server-side applications and services using runtime environments - Node.js, deno and bun. */ 
+
+
+//Old Names of Js
+//Mocha === LiveScript === Finally JavaScript
+
