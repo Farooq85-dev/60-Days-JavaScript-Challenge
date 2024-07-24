89edc1e67d7699acd6ab1ed7a9c1cee5aad3c71c
Muhammad Farooq <famuhammad907@gmail.com>
2024-07-24 09:44:47 +0500

    Day-5

---
+++ Day-5/script.js
@@ -0,0 +1,12 @@
+/* Operator precedence in JavaScript determines the order in which different operators are
+evaluated in an expression. Understanding operator precedence is crucial for writing correct and predictable code.*/
+
+const add = 2 + 3 * 4;
+console.log(add); //Output will be 14
+
+const add1 = (2 + 3) * 4;
+console.log(add); //Output will be 20 due to higher precedence of Parenthesis
+
+const bol = true;
+
+console.log(typeof bol); //To chack data type is boolean
