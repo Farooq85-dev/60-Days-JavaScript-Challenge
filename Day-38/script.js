bf4b5e747c8b8585d70087bd6d5b79b8fce96f24
Muhammad Farooq <irfankhans786787788@gmail.com>
2024-08-29 06:37:33 +0500

    Day-38

---
+++ Day-38/script.js
@@ -0,0 +1,27 @@
+/* ------- Template Literals ------- */
+
+/* Template literals are a feature in JavaScript that were introduced with ES6. They give you a more flexible and maintainable way of working with strings in JavaScript.*/
+
+//What are Template Literals?
+/*
+Template literasl are a feature in JavaScript that let developers work with strings in a convenient way. You denote regular strings in JavaScript using double quotes "" or single quotes ''.*/
+
+const website = "https://farooqfolio.vercel.app/";
+const message = `Welcome to ${website}`;
+
+console.log(message);
+
+/* ------- Template Literals vs Regular Strings ------- */
+
+// String Concatenation
+// Multi-line Strings
+// Readability and Maintainability
+// Generating HTML Markup
+// Creating Dynamic SQL Queries
+// Localization and Internalization
+
+/* ------- Conclusion ------- */
+
+/*
+Template literals provide a convenient way to work with strings in JavaScript.
+You also learned about an advanced feature of template literals: tagged template literals. These are functions that take in an array of string blocks and expressions. They return a string based on the logic you write for the function.*/
